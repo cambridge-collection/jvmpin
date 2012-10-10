b677a0175ec73d593525164fd95413c92558f430
Benjamin Conlan <bjconlan@hotmail.com>
2012-10-10 17:57:32 +1000

    updated test to work with latest api changes

--- test/jvmpin-test.js
+++ test/jvmpin-test.js
@@ -1,14 +1,14 @@
 /*
  * JVMPin - Nailgun protocol implementation
  *
+ * Test cases for API version 1.x
+ *
  * Copyright(c) 2012 Foldr
  * EPL Licensed
  */
-
-
 var jvmpin = require('../lib/jvmpin');
 
-var client = jvmpin.Create();
+var client = jvmpin.createConnection({port: 2113, cmd:'io.foldr.ngtesthost.Stdout'}, function() { console.log('ok'); });
 
 client.on('data', function(data) {
 	console.log(data.toString());
@@ -22,7 +22,3 @@ client.on('error', function(e) {
 	console.log('error');
 	console.log(e);
 });
-
-client.connect_('io.foldr.ngtesthost.Stdout', function() {
-		console.log('connected');
-	});
