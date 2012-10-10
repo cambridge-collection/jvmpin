d85e69e4514b420df8461ecaa3293a0060985636
Benjamin Conlan <bjconlan@hotmail.com>
2012-10-10 23:09:21 +1000

    started basic work on reading input and handling it

--- test/jvmpin-test.js
+++ test/jvmpin-test.js
@@ -8,17 +8,49 @@
  */
 var jvmpin = require('../lib/jvmpin');
 
-var client = jvmpin.createConnection({port: 2113, cmd:'io.foldr.ngtesthost.Stdout'}, function() { console.log('ok'); });
+function basicArgumentsTest() {
+	var client = jvmpin.createConnection({
+		port: 2113,
+		cmd:'io.foldr.ngtesthost.Stdout'
+	}, function() {
+		console.log('ok');
+	});
 
-client.on('data', function(data) {
-	console.log(data.toString());
-});
+	client = jvmpin.createConnection('io.foldr.ngtesthost.Stdout');
 
-client.on('end', function() {
-	console.log('client disconnected');
-});
+	client = jvmpin.createConnection('io.foldr.ngtesthost.Stdout', function() {
+		console.log('ok');
+	});
 
-client.on('error', function(e) {
-	console.log('error');
-	console.log(e);
-});
+	client.on('data', function(data) {
+		console.log(data.toString());
+	});
+
+	client.on('end', function() {
+		console.log('client disconnected');
+	});
+
+	client.on('error', function(e) {
+		console.error('error');
+	});
+}
+
+function basicChunkReading() {
+	var client = jvmpin.createConnection('io.foldr.ngtesthost.Stdout')
+		.on('data', function(data) {
+			var chunks = jvmpin.readChunks(data);
+			chunks.forEach(function(chunk) {
+				switch (chunk.type) {
+					case jvmpin.CHUNK_TYPE.STDOUT: console.log(chunk.data); break;
+					case jvmpin.CHUNK_TYPE.STDERR: console.error(chunk.data); break;
+					default: console.log(chunk.type + " - " + chunk.data,toString());
+				}
+			});
+		}).on('end', function() {
+			console.log('disconnected from server');
+		}).on('error', function(e) {
+			console.log('connection error', e);
+		});
+}
+
+basicChunkReading();
