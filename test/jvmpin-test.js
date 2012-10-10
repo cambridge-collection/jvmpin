3d5e4840cbd14d6c17720937d30635558719ec94
Benjamin Conlan <bjconlan@hotmail.com>
2012-10-10 16:10:29 +1000

    updated code to be simpler and cleaner

--- test/jvmpin-test.js
+++ test/jvmpin-test.js
@@ -5,15 +5,10 @@
  * EPL Licensed
  */
 
+
 var jvmpin = require('../lib/jvmpin');
 
-
-var client = jvmpin.connect({
-	port: 2113,
-	host: 'localhost',
-	cmd: 'io.foldr.ngtesthost.Stdout'}, function() {
-		console.log('connected');
-	});
+var client = jvmpin.Create();
 
 client.on('data', function(data) {
 	console.log(data.toString());
@@ -23,6 +18,11 @@ client.on('end', function() {
 	console.log('client disconnected');
 });
 
-client.on('error', function() {
+client.on('error', function(e) {
 	console.log('error');
-})
+	console.log(e);
+});
+
+client.connect_('io.foldr.ngtesthost.Stdout', function() {
+		console.log('connected');
+	});
